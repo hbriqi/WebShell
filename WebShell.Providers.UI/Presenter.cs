0802584e26a220c9fb0c4b691b2a1cf51afbcc7c
hisham-elbreky <hisham.elbreky@hotmail.com>
2010-08-07 23:22:52 +0300

    Presenter.GetViewHTML

--- WebShell.Providers.UI/Presenter.cs
+++ WebShell.Providers.UI/Presenter.cs
@@ -6,6 +6,7 @@ using WebShell.ClassLibrary.Classes;
 using System.Web;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace WebShell.Providers.UI
 {
@@ -14,9 +15,28 @@ namespace WebShell.Providers.UI
         public IResult GetViewHTML(string resourceName)
         {
             IResult result= null;
-            
+            //TODO: add try catch blocks => High
+            string rPath=HttpContext.Current.Server.MapPath("html/"+resourceName);
+            string strResourceText = File.ReadAllText(rPath);
+            string strFinalText = ParsResourceText(strResourceText);
             return result; 
             
+        }
+
+        private string ParsResourceText(string strResourceText)
+        {
+            SubstituteInclude(strResourceText);
+            if (Regex.Match(strResourceText, "\\{%\\s?extends \\\"\\w*\\.?\\w*\\\"\\s?%\\}").Length > 0)
+            {
+            }
+
+            //TODO: remove string.empty
+            return string.Empty;
+        }
+
+        private void SubstituteInclude(string strResourceText)
+        {
+            throw new NotImplementedException();
         }
 
         public void SetViewModel(dynamic viewType, HttpRequest httpRequest)
